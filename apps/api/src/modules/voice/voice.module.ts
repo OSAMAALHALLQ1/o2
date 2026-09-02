9ea04e4c6542815f124e184763dbb4ce45d653eb
OSAMAALHALLQ1 <osamaalhallqast@gmail.com>
2026-09-02 18:23:30 +0300

    fix(api): wire auth dependencies into voice module

--- apps/api/src/modules/voice/voice.module.ts
+++ apps/api/src/modules/voice/voice.module.ts
@@ -3,9 +3,10 @@ import { PrismaModule } from '../../prisma/prisma.module';
 import { RealtimeModule } from '../realtime/realtime.module';
 import { VoiceController } from './voice.controller';
 import { VoiceService } from './voice.service';
+import { AuthModule } from '../../auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, RealtimeModule],
+  imports: [PrismaModule, RealtimeModule, AuthModule],
   controllers: [VoiceController],
   providers: [VoiceService],
   exports: [VoiceService],
